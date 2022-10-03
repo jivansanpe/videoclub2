9edc8909c9ba5a54b3eeb687ec5fc3005b284688
José Iván Santana Pérez <joseivansantanaperez@alumno.ieselrincon.es>
2022-10-03 12:25:41 +0100

    Second commit

---
+++ database/DATABASE.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bd_videoclub
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pelicula`
+--
+
+DROP TABLE IF EXISTS `pelicula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pelicula` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(45) NOT NULL,
+  `fecha` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelicula`
+--
+
+LOCK TABLES `pelicula` WRITE;
+/*!40000 ALTER TABLE `pelicula` DISABLE KEYS */;
+INSERT INTO `pelicula` VALUES (1,'Regreso al futuro','1985'),(2,'En busca del arca perdida','1981'),(3,'Matrix','1999'),(9,'El Padrino','1972'),(16,'adawd','awdawd'),(20,'El Padrino','1972'),(21,'asxcascasdc','asdcsdcsdc');
+/*!40000 ALTER TABLE `pelicula` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-03  2:17:39
--- frontend/ionic-rest-api/src/app/services/pelicula-crud.service.ts
+++ frontend/ionic-rest-api/src/app/services/pelicula-crud.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { Pelicula } from '../model/pelicula';
+import { Pelicula } from '../interfaces/pelicula';
 
 
 
